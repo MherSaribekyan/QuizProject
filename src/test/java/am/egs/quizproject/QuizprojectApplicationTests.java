3576c2fb9f6e5b3ea5a6df193c29c2e77f4c2144
Mher Saribekyan <saribekyan3233@gmail.com>
2020-01-23 21:26:13 +0400

    initial commit

---
+++ src/test/java/am/egs/quizproject/QuizprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package am.egs.quizproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
